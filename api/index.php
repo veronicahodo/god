a50d9102dc20420b018d438e3f897043ec1c72a7
Veronica Hodo <veronica@hodo.llc>
2023-09-02 23:36:08 -0700

    Updated index.php

--- api/index.php
+++ api/index.php
@@ -6,38 +6,64 @@
 
 
 require_once('vcrud.php');
+require_once('vuser.php');
 require_once('node.php');
 require_once('link.php');
 require_once('config.php');
 
 
-$crud = new VCRUD($DB_USER,$DB_PASS,$DB_HOST,$DB_NAME);
+$crud = new VCRUD($DB_USER, $DB_PASS, $DB_HOST, $DB_NAME);
 
 $output = [
-	'status'=>'error',
-	'message'=>'Undefined error'
+	'status' => 'error',
+	'message' => 'Undefined error'
 ];
 
+function processAuthLogin(VCRUD $c)
+{
+	$username = htmlspecialchars($_REQUEST['username'] ?? '');
+	$password = htmlspecialchars($_REQUEST['password'] ?? '');
+	$token = new VTOKEN();
+	$user = new VUSER();
+	if ($userId = $user->validatePassword($username, $password, $c)) {
+		$token = $token->generateToken($userId, $c);
+		return [
+			'status' => 'ok',
+			'userId' => $userId,
+			'token' => $token
+		];
+	} else {
+		return [
+			'status' => 'error',
+			'message' => '[auth.login] username and password do not match'
+		];
+	}
+}
+
+
 $command = htmlspecialchars($_REQUEST['command'] ?? '');
+
+
+
 
 switch (strtolower($command)) {
 	case 'node.read':
 		$node = new NODE();
 		$output = $node->processRead($crud);
 		break;
+	case 'auth.login':
+		$output = processAuthLogin($crud);
+		break;
 	case 'test':
 		$output = [
-			'status'=>'ok',
-			'message'=>'test command complete'
+			'status' => 'ok',
+			'message' => 'test command complete'
 		];
 		break;
 	default:
-		$output['message'] = 'Invalid command specified:'.$command;
-
+		$output['message'] = 'Invalid command specified:' . $command;
 }
 
 
 header('Content-type: application/json');
 print(json_encode($output));
-
-
